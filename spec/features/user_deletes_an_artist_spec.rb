bbc6e7ebe935b3e6eb9dd53988bd434324233326
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-12-18 12:54:25 -0700

    implement artist CRUD functionality

---
+++ spec/features/user_deletes_an_artist_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "User deletes an existing artist" do
+  scenario "they are redirected to the artists index and do not see the artist name" do
+    artist        = Artist.create(name: "Bob Marley", image_path: "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg")
+
+    visit artist_path(artist)
+    click_on "Delete"
+
+    expect(page).to have_content artist.name
+    expect(page).to_not have_css("img[src=\"#{artist.image_path}\"]")
+  end
+end
---
+++ spec/features/user_edits_an_artist_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User edits an existing artist" do
+  scenario "they see the updated data for the individual artist" do
+    artist        = Artist.create(name: "Bob Marley", image_path: "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg")
+    updated_name  = "Beyonce"
+
+    visit artist_path(artist)
+    click_on "Edit"
+    fill_in "artist_name", with: updated_name
+    click_on "Update Artist"
+
+    expect(page).to have_content updated_name
+    expect(page).to_not have_content artist.name
+    expect(page).to have_css("img[src=\"#{artist.image_path}\"]")
+  end
+end
